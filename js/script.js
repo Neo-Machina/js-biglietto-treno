2b2a6c97a3e0a26fce305f097004613d55f66fb1
Neo-Machina <andreistatache1997@gmail.com>
2022-06-01 17:31:57 +0200

    script.js completed

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// Numero di chilometri che vuole percorrere l'utente
+const numberKm = parseInt ( prompt ('Dimmi il numero di chilometri che vuoi percorrere') );
+// Età del passegero
+const userAge = parseInt ( prompt ('Dimmi la tua età') );
+
+// Prezzo standard del biglietto
+const standardPrice = numberKm * 0.21;
+// console.log(standardPrice);
+
+// Sconto biglietti per minorenni e per gli over 65
+let discountPrice;
+
+if (userAge < 18) {
+    discountPrice = standardPrice * 20/100;
+} else if (userAge > 65) {
+    discountPrice = standardPrice * 40/100;
+} else {
+    discountPrice = standardPrice;
+}
+// console.log(discountPrice)
+
+// Prezzo totale del biglietto
+let finalPrice;
+
+if (userAge < 18 || userAge > 65) {
+    finalPrice = standardPrice - discountPrice;
+} else {
+    finalPrice = standardPrice;
+}
+// console.log(finalPrice);
+
+// Output prezzo finale
+document.getElementById('final-price').innerHTML = 'Il prezzo finale del biglitto è ' + finalPrice.toFixed(2);
+
